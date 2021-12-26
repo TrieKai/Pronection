95d1a581f532903a2871166ee235021b08aa54c1
TrieKai <trie55432@gmail.com>
2021-12-26 13:28:31 +0800

    [feat]: init firebase and add some svg

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { FirebaseInit } from 'util/firebase'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  FirebaseInit()
+
   return <Component {...pageProps} />
 }
 
