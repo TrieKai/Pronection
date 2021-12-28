1c14d0aecfa4612ae8f69f7e3c3750ba9c6c6469
TrieKai <trie55432@gmail.com>
2021-12-28 22:34:44 +0800

    fix(storybook): svg ReactComponent

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+
 module.exports = {
   stories: [
     '../stories/**/*.stories.mdx',
@@ -5,5 +7,17 @@ module.exports = {
     '../components/**/*.stories.@(js|jsx|ts|tsx)'
   ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  framework: '@storybook/react'
+  framework: '@storybook/react',
+  webpackFinal: async config => {
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin()
+    ]
+    config.module.rules.unshift({
+      test: /\.svg$/,
+      use: [{ loader: '@svgr/webpack' }]
+    })
+
+    return config
+  }
 }
