3bdc5e1cb58f0d52af022200be345c7b7152d8e8
TrieKai <trie55432@gmail.com>
2022-03-26 23:44:06 +0800

    chore(typescript): add typescript of FirebaseOptions

--- util/firebase/firebase.ts
+++ util/firebase/firebase.ts
@@ -1,6 +1,6 @@
-import { getApp, initializeApp } from 'firebase/app'
+import { FirebaseOptions, getApp, initializeApp } from 'firebase/app'
 
-const firebaseConfig = {
+const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
